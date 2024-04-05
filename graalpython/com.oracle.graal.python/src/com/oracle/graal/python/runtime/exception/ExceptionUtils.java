9cb5ef69c3ff45d9fb13ab2cebddc0697f362f91
Matt D'Souza <matt.dsouza@oracle.com>
2024-04-05 15:43:26 -0400

    Update to use new introspection API

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/runtime/exception/ExceptionUtils.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/runtime/exception/ExceptionUtils.java
@@ -56,8 +56,6 @@ import com.oracle.graal.python.builtins.objects.traceback.LazyTraceback;
 import com.oracle.graal.python.nodes.BuiltinNames;
 import com.oracle.graal.python.nodes.bytecode.BytecodeFrameInfo;
 import com.oracle.graal.python.nodes.bytecode.FrameInfo;
-import com.oracle.graal.python.nodes.bytecode_dsl.BytecodeDSLFrameInfo;
-import com.oracle.graal.python.nodes.bytecode_dsl.PBytecodeDSLRootNode;
 import com.oracle.graal.python.nodes.call.CallNode;
 import com.oracle.graal.python.nodes.exception.TopLevelExceptionHandler;
 import com.oracle.graal.python.nodes.function.BuiltinFunctionRootNode;
@@ -116,7 +114,7 @@ public final class ExceptionUtils {
                 }
 
                 if (bytecodeNode != null) {
-                    return bytecodeNode.getBytecodeLocation(frame, location).findSourceLocation().getStartLine();
+                    return bytecodeNode.getBytecodeLocation(frame, location).getSourceLocation().getStartLine();
                 }
             } else {
                 return ((BytecodeFrameInfo) frameInfo).getLine(frame);
